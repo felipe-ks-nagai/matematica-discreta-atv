29f37ed176c6f19fae3e434759cbdff614d18f8b
felipe <felipe.nagai7@hotmail.com>
2026-05-11 20:26:24 -0300

    adicionando explicacao

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
 calcularPermutacaoSimples = () => {
-  let r;
-  const n = parseInt(document.getElementById('permutacao-simples-input').value)
-  const resultado = document.getElementById('permutacao-simples-formula')
-  r = fatorial(n)
-  resultado.value = `Resultado: ${r}`
+    let r;
+    const n = parseInt(document.getElementById('permutacao-simples-input').value)
+    const resultado = document.getElementById('permutacao-simples-formula')
+    const explicacao = document.getElementById('permutacao-simples-explicacao')
+    r = fatorial(n)
+    resultado.value = `Resultado: ${r}`
+    explicacao.innerHTML = `Explicação: ${n}! = ${r}`
 }
 
 calcularPermutacaoRepeticao = () => {
@@ -12,13 +14,15 @@ calcularPermutacaoRepeticao = () => {
     let kArray = [];
     const n = parseInt(document.getElementById('permutacao-repeticao-input').value)
     const k = parseInt(document.getElementById('permutacao-repeticao-repeticoes-input').value)
-    kArray = k.toString().split(',')
     const resultado = document.getElementById('permutacao-repeticao-formula')
+    const explicacao = document.getElementById('permutacao-repeticao-explicacao')
+    kArray = k.toString().split(',')
     r = fatorial(n);
     kArray.forEach(element => {
         y *= fatorial(parseInt(element));
     });
     r /= y;
+    explicacao.innerHTML = `Explicação: ${n}! / (${kArray.map(x => `${x}!`).join(' × ')}) = ${fatorial(n)} / (${kArray.map(x => `${fatorial(parseInt(x))}`).join(' × ')}) = ${r}`
     resultado.value = `Resultado: ${r}`
 }
 
@@ -27,7 +31,9 @@ calcularCombinacaoSimples = () => {
     const n = parseInt(document.getElementById('combinacao-simples-input-n').value)
     const p = parseInt(document.getElementById('combinacao-simples-input-p').value)
     const resultado = document.getElementById('combinacao-simples-formula')
+    const explicacao = document.getElementById('combinacao-simples-explicacao')
     r = fatorial(n) / (fatorial(p) * fatorial(n - p))
+    explicacao.innerHTML = `Explicação: ${n}! / (${p}! * (${n} - ${p})!)) = ${fatorial(n)} / (${fatorial(p)} * ${fatorial(n - p)}) = ${r}`
     resultado.value = `Resultado: ${r}`
 }
 
@@ -36,7 +42,9 @@ calcularArranjoSimples = () => {
     const n = parseInt(document.getElementById('arranjos-simples-input-n').value)
     const p = parseInt(document.getElementById('arranjos-simples-input-p').value)
     const resultado = document.getElementById('arranjos-simples-formula')
+    const explicacao = document.getElementById('arranjo-simples-explicacao')
     r = fatorial(n) / fatorial(n - p)
+    explicacao.innerHTML = `Explicação: ${n}! / (${n} - ${p})! = ${fatorial(n)} / (${fatorial(n - p)}) = ${r}`
     resultado.value = `Resultado: ${r}`
 }
 
@@ -46,4 +54,14 @@ fatorial = (n) => {
         r *= i
     }
     return r
+}
+
+limpar = (componente, explicacao) => {
+    const resultado = componente.querySelector('input[type="text"]')
+    const inputs = componente.querySelectorAll('input[type="number"]')
+    resultado.value = 'Resultado: '
+    explicacao.textContent = ''
+    inputs.forEach(input => {
+        input.value = ''
+    })
 }
